9cb9e0d125be2f66b918af946e139e8dc1cb587a
Tomasz Dyda <tomasz.dyda.chem@gmail.com>
2023-11-22 21:35:06 +0100

    CV UPDATE STYLES

--- CV/site.js
+++ CV/site.js
@@ -1,5 +1,5 @@
 /******************************************
- *  Author : Author   
+ *  Author : Author
  *  Created On : Mon Nov 20 2023
  *  File : site.js
  *******************************************/
@@ -7,28 +7,45 @@ let breakpoint;
 let body;
 let section;
 let head;
+let download;
+let element;
+let downloadBtn;
 
 const main = () => {
-    prepareDOMElemens()
-    prepareDOMEvents()
-}
+  prepareDOMElemens();
+  prepareDOMEvents();
+};
 
 const prepareDOMElemens = () => {
-    breakpoint = document.querySelector(".info_header")
-    body = document.querySelector("body")
-    section = document.querySelector("section")
-    head = document.querySelector(".head")  
-}
+  breakpoint = document.querySelector(".info_header");
+  body = document.querySelector("body");
+  section = document.querySelector("section");
+  head = document.querySelector(".head");
+  download = document.querySelector(".download");
+  element = section;
+};
 
 const prepareDOMEvents = () => {
-    breakpoint.addEventListener("click", changeStyle)
-}
+  breakpoint.addEventListener("click", changeStyle);
+  download.addEventListener("click", saveAsPDF);
+};
 
 const changeStyle = () => {
-    body.classList.toggle("body")
-    section.classList.toggle("section-width")
-    head.classList.toggle("head1")
+  body.classList.toggle("body");
+  section.classList.toggle("section-width");
+  head.classList.toggle("head1");
+};
 
-}
+const saveAsPDF = () => {
+  let opt = {
+    margin: 0,
+    filename: "myfile.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 5 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
 
-document.addEventListener("DOMContentLoaded", main)
+  html2pdf().set(opt).from(section).save();
+};
+
+document.addEventListener("DOMContentLoaded", main);
