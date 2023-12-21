3c669b8a2917a62f1765bf86f9216dfd9173b40c
Tomek <tomasz.dyda.chem@gmail.com>
2023-12-21 15:29:52 +0100

    add dynamic search and popup inputs

--- LabStore/LabStore/wwwroot/js/site.js
+++ LabStore/LabStore/wwwroot/js/site.js
@@ -7,169 +7,238 @@
 let inputs;
 let getValves;
 let dataList;
-const URL = 'http://localhost:5000/api/Valves';
+const URL = "http://localhost:5000/api/Valves";
 let formEl;
 let editBtn;
 let popup;
 let closeBtn;
-let editValves = document.querySelector('.editValves');
+let editValves;
 let dataTable;
+let searchInput;
+let applyBtn;
+let cancelBtn;
 
 const main = () => {
-	prepareDOMElements();
-	prepareDOMEvents();
+  prepareDOMElements();
+  prepareDOMEvents();
 };
 
 const prepareDOMElements = () => {
-	inputs = document.querySelectorAll('input');
-	getValves = document.querySelector('.getValves');
-	dataList = document.querySelector('.valvesList');
-	formEl = document.querySelector('.valveForm');
-	editBtn = document.querySelectorAll('.editBtn');
-	popup = document.querySelector('.popup');
-	closeBtn = document.querySelector('.x-mark');
-	// dataTable = document.querySelector('.dataTable');
+  inputs = document.querySelectorAll("input");
+  getValves = document.querySelector(".getValves");
+  dataList = document.querySelector(".valvesList");
+  formEl = document.querySelector(".valveForm");
+  editBtn = document.querySelectorAll(".editBtn");
+  popup = document.querySelector(".popup");
+  closeBtn = document.querySelector(".x-mark");
+  searchInput = document.querySelector(".search-input");
+  editValves = document.querySelector(".editValves");
+
+  // dataTable = document.querySelector('.dataTable');
 };
 
 const prepareDOMEvents = () => {
-	for (let i = 0; i < inputs.length; i++) {
-		inputs[i].addEventListener('focus', focusInput);
-		inputs[i].addEventListener('blur', blurInput);
-	}
-	getValves.addEventListener(
-		'click',
-		() => {
-			getValvesList(dataList);
-		},
-		{ once: true }
-	);
-	formEl.addEventListener('submit', addNewValve);
-	dataList.addEventListener('click', checkClick);
-	popup.addEventListener('click', checkClick);
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("focus", focusInput);
+    inputs[i].addEventListener("blur", blurInput);
+  }
+  getValves.addEventListener("click", () => addHeaders(dataList), {
+    once: true,
+  });
+  getValves.addEventListener(
+    "click",
+    () => {
+      getValvesList(dataList);
+    },
+    { once: true }
+  );
+  formEl.addEventListener("submit", addNewValve);
+  dataList.addEventListener("click", checkClick);
+  popup.addEventListener("click", checkClick);
+  searchInput.addEventListener("input", dynamicSearch);
 };
 
 const focusInput = (e) => {
-	e.target.style.backgroundColor = 'rgb(72, 89, 138)';
+  e.target.style.backgroundColor = "rgb(72, 89, 138)";
 };
 
 const blurInput = (e) => {
-	e.target.style.backgroundColor = 'transparent';
+  e.target.style.backgroundColor = "transparent";
 };
 
 const getValvesList = (list) => {
-	fetch(URL, {
-		method: 'GET',
-	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((data) => {
-			data.forEach((x) => {
-				array = [
-					(index = x._index),
-					(name = x._fullName),
-					(supplier = x._supplier),
-					(acceptanceDate = x._acceptanceDate),
-					(expiriationDate = x._expiriationDate),
-					(amount = x._amount),
-					(storagePlace = x._storagePlace),
-					(tubeLength = x._tubeLength),
-					(destiny = x._destiny),
-				];
-				addToList(array, list);
-			});
-		});
+  fetch(URL, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      data.forEach((x) => {
+        array = [
+          (index = x._index),
+          (name = x._fullName),
+          (supplier = x._supplier),
+          (acceptanceDate = x._acceptanceDate),
+          (expiriationDate = x._expiriationDate),
+          (amount = x._amount),
+          (storagePlace = x._storagePlace),
+          (tubeLength = x._tubeLength),
+          (destiny = x._destiny),
+        ];
+        addToList(array, list);
+      });
+    });
+};
+
+const addHeaders = (list) => {
+  let headers = [
+    (index = "Index"),
+    (fullName = "Full name"),
+    (supplier = "Supplier"),
+    (acceptanceDate = "Acceptance Date"),
+    (expiriationDate = "Expiriation Date"),
+    (amount = "Amount"),
+    (storagePlace = "Storage Place"),
+    (tubeLength = "Tube Length"),
+    (destiny = "Destiny"),
+  ];
+
+  let liElement = document.createElement("li");
+  let headersList = document.createElement("ul");
+  let container = document.createElement("div");
+
+  headersList.classList.add("headersList");
+  container.classList.add("container");
+
+  headers.forEach((item) => {
+    let header = document.createElement("li");
+    header.textContent = item;
+
+    headersList.appendChild(header);
+  });
+
+  container.appendChild(headersList);
+  liElement.appendChild(container);
+  list.appendChild(liElement);
 };
 
 const addToList = (dataValuesArray, list) => {
-	let liElement = document.createElement('li');
-	let dataList = document.createElement('ul');
-	let container = document.createElement('div');
+  let liElement = document.createElement("li");
+  let dataList = document.createElement("ul");
+  let container = document.createElement("div");
 
-	dataList.classList.add('dataList');
-	container.classList.add('container');
+  dataList.classList.add("dataList");
+  container.classList.add("container");
 
-	for (let i = 0; i < dataValuesArray.length; i++) {
-		let dataValue = document.createElement('p');
-		let liParameter = document.createElement('li');
+  for (let i = 0; i < dataValuesArray.length; i++) {
+    let liParameter = document.createElement("li");
 
-		dataValue.textContent = dataValuesArray[i];
+    liParameter.textContent = dataValuesArray[i];
 
-		liParameter.appendChild(dataValue);
-		dataList.appendChild(liParameter);
-	}
+    dataList.appendChild(liParameter);
+  }
 
-	container.appendChild(dataList);
-	liElement.appendChild(container);
-	list.appendChild(liElement);
+  container.appendChild(dataList);
+  liElement.appendChild(container);
+  list.appendChild(liElement);
 
-	createToolArea(container);
+  createToolArea(container);
 };
 
 const createToolArea = (newElement) => {
-	let editBtn = document.createElement('button');
-	let deleteBtn = document.createElement('button');
-	let toolsArea = document.createElement('div');
+  let editBtn = document.createElement("button");
+  let deleteBtn = document.createElement("button");
+  let toolsArea = document.createElement("div");
 
-	editBtn.classList.add('editBtn');
-	deleteBtn.classList.add('deleteBtn');
+  editBtn.classList.add("editBtn");
+  deleteBtn.classList.add("deleteBtn");
 
-	editBtn.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
-	deleteBtn.innerHTML = "<i class='fa-solid fa-trash'></i>";
+  editBtn.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
+  deleteBtn.innerHTML = "<i class='fa-solid fa-trash'></i>";
 
-	toolsArea.classList.add('tools');
+  toolsArea.classList.add("tools");
 
-	toolsArea.append(editBtn, deleteBtn);
-	newElement.append(toolsArea);
+  toolsArea.append(editBtn, deleteBtn);
+  newElement.append(toolsArea);
 };
 
 const addNewValve = (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	const formData = new FormData(formEl);
+  const formData = new FormData(formEl);
 
-	const data = Object.fromEntries(formData);
-	console.log(data);
+  const data = Object.fromEntries(formData);
+  console.log(data);
 
-	fetch('http://localhost:5000/api/Valves', {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(data),
-	});
+  fetch("http://localhost:5000/api/Valves", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 };
 
 const checkClick = (e) => {
-	if (e.target.matches('.editBtn')) {
-		edit(e);
-	} else if (e.target.matches('.closePopup')) {
-		closePopup(e);
-	}
+  if (e.target.matches(".editBtn")) {
+    edit(e);
+  } else if (e.target.matches(".closePopup")) {
+    closePopup(e);
+  }
 };
 
 const edit = (e) => {
-	popup.setAttribute('style', 'display: block');
-	editValve(e);
+  popup.setAttribute("style", "display: block");
+  editValve(e);
 };
 
 const editValve = (e) => {
-	liElements = e.target.closest('li').firstChild.querySelectorAll('input');
-	liElements.forEach((element) => {
-		let editInput = document.createElement('input');
-		editInput.value = element.value;
-		editValves.appendChild(editInput);
-	});
+  liElements = e.target
+    .closest("li")
+    .firstChild.firstChild.querySelectorAll("li");
+  console.log(editValves);
+
+  liElements.forEach((element) => {
+    let editInput = document.createElement("input");
+    editInput.value = element.textContent;
+    editValves.appendChild(editInput);
+  });
+
+  //   applyBtn = document.createElement("button");
+  //   closeBtn = document.createElement("button");
+
+  //   applyBtn.classList.add("apply-btn");
+  //   cancelBtn.classList.add("cancel-btn");
 };
 
 const closePopup = () => {
-	popup.setAttribute('style', 'display: none');
-	oldData = editValves.querySelectorAll('input'); // czyszczenie popupa
-	oldData.forEach((element) => element.remove());
+  popup.setAttribute("style", "display: none");
+  oldData = editValves.querySelectorAll("input"); // czyszczenie popupa
+  oldData.forEach((element) => element.remove());
 };
 
-document.addEventListener('DOMContentLoaded', main);
+const dynamicSearch = (e) => {
+  const searchTerm = e.target.value.toUpperCase();
+  const listItems = document.querySelectorAll(".dataList li");
+  //   console.log(listItems);
+
+  let searchInputArray = [];
+
+  listItems.forEach(function (item) {
+    const itemText = item.textContent.toUpperCase();
+
+    if (itemText.includes(searchTerm)) {
+      searchInputArray.push(item.closest(".container")); //dodajemy element który ma się wyświetlać do tablicy którą wyświetlimy po zakończeniu przeszukiwania
+    } else {
+      item.closest("div").style.display = "none";
+    }
+  });
+  searchInputArray.forEach((item) => (item.style.display = "flex"));
+};
+
+document.addEventListener("DOMContentLoaded", main);
 
 // function GetId() {
 //   let id = document.getElementById("valveId").value;
